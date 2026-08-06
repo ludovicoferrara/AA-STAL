b0e10c0130f34c791e5532b88d3e8e8fb72d4ece
ludovicoferrara <ludovicoferrara5@gmail.com>
2026-08-06 10:10:06 +0200

    Refactoring: removing useless code

--- data_pipeline/src/third-party/hands23_beta/hodetector/data/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-# ensure the builtin datasets are registered
-from .ho import load_ho_voc_instances, register_ho_pascal_voc
-
-from .ho_transforms import hoMapper
--- data_pipeline/src/third-party/hands23_beta/hodetector/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-from . import model_zoo  # registers pathhandlers
-from . import positional_encoding
